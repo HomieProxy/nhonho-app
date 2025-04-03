336e5682e6d2b68c0e5ae71507d9914664b7ddb3
zoro <de.ztyle.tkh@gmail.com>
2025-04-04 01:06:49 +0800

    test1

--- lib/features/panel/xboard/services/http_service/domain_service.dart
+++ lib/features/panel/xboard/services/http_service/domain_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class DomainService {
   static const String ossDomain =
-      'https://github.com/pandaaxi/nhonho-app/blob/main/oss.json';
+      'https://raw.githubusercontent.com/pandaaxi/nhonho-app/refs/heads/main/oss.json';
 
 // 从返回的 JSON 中挑选一个可以正常访问的域名
   static Future<String> fetchValidDomain() async {
